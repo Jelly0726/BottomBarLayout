018414d09418481b71ddd811874933d2bd57e7bc
Jelly0726 <18659464090@163.com>
2017-09-13 10:27:15 +0800

    Update BottomBarItem.java

--- library/src/main/java/com/chaychan/library/BottomBarItem.java
+++ library/src/main/java/com/chaychan/library/BottomBarItem.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
